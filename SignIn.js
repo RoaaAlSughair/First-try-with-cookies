a3e105678716e247e0a4d43832d0d664233ec09e
RuaaAlsugheir <alsugheirruaa@gmail.com>
2023-03-22 11:55:35 +0300

    Getting and validating cookies

--- SignIn.js
+++ SignIn.js
@@ -1,6 +1,23 @@
-let email;
-let password;
-let isRemembered = false;
+const getCookie = (name) => {
+  const cookieArr = document.cookie.split(';');
+  for (i = 0; i < cookieArr.length; i++) {
+    const cookiePair = cookieArr[i].split('=');
+    if (name === cookiePair[0].trim()) {
+      return cookiePair[1];
+    }
+  }
+  return null;
+};
+
+let email = getCookie('email');
+let password = getCookie('password');
+let isRemembered = getCookie('is_remembered') || false;
+
+if (isRemembered) {
+  document.querySelector(`input[type = email]`).defaultValue = email;
+  document.querySelector(`input[type = password]`).defaultValue = password;
+  document.querySelector(`input[type = checkbox]`).defaultValue = isRemembered;
+}
 
 const handleEmail = (e) => {
   email = e.target.value;
@@ -10,6 +27,18 @@ const handlePassword = (e) => {
 };
 const handleIsRemembered = (e) => {
   isRemembered = e.target.checked;
+};
+
+const validateCookie = (cookie) => {
+  const http = new XMLHttpRequest();
+  const data = cookie;
+  http.open('POST', 'https://core.talentspace.ai/api2/verify_token', true);
+  http.send(data);
+  http.onreadystatechange = () => {
+    if (http.status === 200 && http.readyState === 4) {
+      console.log('Success');
+    } else console.log('Failed');
+  };
 };
 
 const handleSubmit = (e) => {
@@ -24,14 +53,15 @@ const handleSubmit = (e) => {
   http.onreadystatechange = () => {
     if (http.status === 200 && http.readyState === 4) {
       console.log(http.response);
-      document.cookie = `message=Logged_In Max-Age=${5 * 60 * 1000}`;
-    } else console.log("failed");
+      document.cookie = `message=Logged_In Max-Age=${5 * 60}`;
+      validateCookie(getCookie('message'));
+    } else console.log('failed');
   };
 
   if (isRemembered) {
-    document.cookie = `email=${email} Max-Age=${5 * 60 * 1000}`;
-    document.cookie = `password=${password} Max-Age=${5 * 60 * 1000}`;
-    document.cookie = `isRemembered = ${isRemembered} Max-Age=${5 * 60 * 1000}`;
+    document.cookie = `email=${email} Max-Age=${5 * 60}`;
+    document.cookie = `password=${password} Max-Age=${5 * 60}`;
+    document.cookie = `isRemembered = ${isRemembered} Max-Age=${5 * 60}`;
   }
 };
 
